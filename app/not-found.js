a3a1072364b4454ad72ccca9ca8cd948d56d6aae
Harry Wynn <harrywynn@gmail.com>
2026-03-16 11:05:43 -0400

    added custom 404 and 500 pages/images

---
+++ app/not-found.js
@@ -0,0 +1,27 @@
+'use client';
+
+import ErrorStatePage from '@/app/components/error-state-page';
+
+export default function NotFound() {
+	return (
+		<ErrorStatePage
+			statusCode="404"
+			title="Page Not Found"
+			subtitle="This trail went cold."
+			description="The page you requested is not here, has moved, or never existed in this workspace."
+			imageSrc="/error-404.png"
+			imageAlt="Hire Gnome 404 illustration"
+			primaryAction={{
+				label: 'Go Home',
+				href: '/',
+				iconKind: 'home'
+			}}
+			secondaryAction={{
+				label: 'Back',
+				onClick: () => window.history.back(),
+				variant: 'secondary',
+				iconKind: 'back'
+			}}
+		/>
+	);
+}
